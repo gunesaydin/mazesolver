462ec7e9f7e73f8c10fcdab23734df213b1c46f7
gunesaydin <gunes.a@gmail.com>
2025-04-28 16:14:08 +0300

    Add window class constructor

---
+++ window.py
@@ -0,0 +1,14 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        
+        self.root = Tk()
+        self.root.title = "Maze Solver"
+
+        self.canvas = Canvas()
+        self.canvas.pack()
+
+        self.running = False
