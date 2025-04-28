a85b7553aa4bdfd7242933a865bd428ccaef7065
gunesaydin <gunes.a@gmail.com>
2025-04-28 17:29:08 +0300

    Add main.py, add initial rendering logic

--- window.py
+++ window.py
@@ -2,13 +2,27 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window():
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
         
-        self.root = Tk()
-        self.root.title = "Maze Solver"
+        self.__root = Tk()
+        self.__root.title = "Maze Solver"
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
-        self.canvas = Canvas()
-        self.canvas.pack()
+        self.__canvas = Canvas()
+        self.__canvas.pack()
 
-        self.running = False
+        self.__running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+
+        while self.__running:
+            self.redraw()
+
+    def close(self):
+        self.__running = False
